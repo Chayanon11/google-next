f3a822537dfa3138fcaa70dcd49b006b8be1d8d9
Neung0511 <neung@gmail.com>
2024-04-22 18:37:14 +0700

    fix bug for deploy 02

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,6 +1,5 @@
+import WebSearchResults from "../../components/WebSearchResults";
 import Link from "next/link";
-import WebSearchResults from "@/app/components/WebSearchResults";
-import { Suspense } from "react";
 
 export default async function WebSearchPage({ searchParams }) {
   const startIndex = searchParams.start || "1";
@@ -8,33 +7,25 @@ export default async function WebSearchPage({ searchParams }) {
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&start=${startIndex}`
   );
-
   if (!response.ok) throw new Error("Something went wrong");
-
   const data = await response.json();
   const results = data.items;
 
   if (!results) {
     return (
-      <div className="flex flex-col justify-center items-center pt-10 ">
+      <div className="flex flex-col justify-center items-center pt-10">
         <h1 className="text-3xl mb-4">
           No results found for {searchParams.searchTerm}
         </h1>
         <p className="text-lg">
           Try searching the web or images for something else{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
         </p>
-        <Link href="/" className="text-blue-500">
-          Home
-        </Link>
       </div>
     );
   }
 
-  return (
-    <>
-      <Suspense>
-        <div>{results && <WebSearchResults results={data} />}</div>
-      </Suspense>
-    </>
-  );
+  return <div>{results && <WebSearchResults results={data} />}</div>;
 }
